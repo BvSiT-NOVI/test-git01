a5e422ccc4c1f284fb90b1cd056b3562346a0e79
BvSiT <34773187+BvSiT@users.noreply.github.com>
2021-06-13 22:06:15 +0200

    Correct mijnantwoord

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,4 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("In mijnantwoord branch");
     }
-}git remote add origin https://github.com/BvSiT-NOVI/test-git01.git
+}

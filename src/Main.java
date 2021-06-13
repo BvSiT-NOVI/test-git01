6d3537c962ad1efd5e4af3f59c2f99cb3629755b
BvSiT <34773187+BvSiT@users.noreply.github.com>
2021-06-13 21:11:38 +0200

    Third commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("In main branch");
+        System.out.println("In mijnantwoord branch");
     }
-}
+}git remote add origin https://github.com/BvSiT-NOVI/test-git01.git
